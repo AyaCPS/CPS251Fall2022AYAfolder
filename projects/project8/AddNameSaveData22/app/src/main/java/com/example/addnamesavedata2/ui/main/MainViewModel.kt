437d6a0bc6bc85d47f8c989aa6f1350848cf1904
aya elzebair <ayael@DESKTOP-RF6DLUP>
2022-10-23 14:30:11 -0400

    update

--- projects/project8/AddNameSaveData22/app/src/main/java/com/example/addnamesavedata2/ui/main/MainViewModel.kt
+++ projects/project8/AddNameSaveData22/app/src/main/java/com/example/addnamesavedata2/ui/main/MainViewModel.kt
@@ -4,8 +4,8 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    var names : MutableLiveData<String> = MutableLiveData(" ")
-    var newName :MutableLiveData<String> = MutableLiveData(" ")
+    var names : MutableLiveData<String> = MutableLiveData("No name display ")
+    var newName :MutableLiveData<String> = MutableLiveData(" Enter name")
 
     fun addName() {
         names.value = names.value + newName.value + "\n"
